b3883bce0919fc77b3f075c8a5a10d577a46bd49
Brady Georgen <pbradygeorgen@me.com>
2025-03-09 17:48:36 -0500

    beginning to port resume framework

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,44 @@
-/** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
+import plugin from 'tailwindcss/plugin';
+
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,css,scss,mdx}'],
   safelist: ['stroke-gray-400'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        yellow: '#efc603',
+      },
+      keyframes: {
+        typing: {
+          '0%, 100%': { width: '0%' },
+          '30%, 70%': { width: '100%' },
+        },
+        blink: {
+          '0%': { opacity: 0 },
+        },
+        'rotate-loader': {
+          '0%': {
+            transform: 'rotate(0deg)',
+            strokeDashoffset: '360%',
+          },
+          '100%': {
+            transform: 'rotate(360deg)',
+            strokeDashoffset: '-360%',
+          },
+        },
+      },
+      screens: {
+        touch: { raw: 'only screen and (pointer: coarse)' },
+      },
+    },
+  },
   plugins: [
+    import('@tailwindcss/forms'),
+    import('@tailwindcss/typography'),
     plugin(function({ addUtilities }) {
-      addUtilities({ '.stroke-gray-400': { stroke: '#9CA3AF' } }, ['responsive', 'hover'])
-    })
+      addUtilities({
+        '.stroke-gray-400': { stroke: '#9CA3AF' },
+      }, ['responsive', 'hover'])
+    }),
   ],
 }
