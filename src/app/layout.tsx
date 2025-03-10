9fd2c055ecb2c24e13183c1ec4429d854046abfb
Brady Georgen <pbradygeorgen@me.com>
2025-03-10 00:38:39 -0500

    loading bento screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,11 @@
-'use client';
-
-import "@/styles/globals.scss";
-import { useEffect } from 'react';
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  useEffect(() => {
-    console.log('Root layout mounted');
-  }, []);
-
   return (
     <html lang="en">
-      <head>
-        <title>p.bradygeorgen</title>
-      </head>
-      <body style={{ margin: 0, padding: 0 }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
